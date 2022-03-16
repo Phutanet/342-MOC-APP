f13da4a2ee1c265677cfa647b6b3acd21d50a36e
pwat16918 <pwat999@hotmail.com>
2022-03-16 22:17:58 +0700

    Page init

--- screens/PriceScreen.js
+++ screens/PriceScreen.js
@@ -7,28 +7,45 @@ import {
   TouchableOpacity,
   View,
   Image,
+  ScrollView,
 } from 'react-native';
 
 const PriceScreen = ({navigation}) => {
   
   return (
-    <SafeAreaView style={styles.container}>
-      
-      <View>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text>
+            Header
+          </Text>
+        </View>
+        <Text style={styles.textTitle}>ราคาสินค้าอุปโภคบริโภค/{"\n"}สินค้าเกษตร</Text>
         <Text>THIS IS View Prices PAGE</Text>
-
       </View>
-
-
-    </SafeAreaView>
+    </ScrollView>  
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor:'white',
+  },
+  header:{
+    width:'100%',
+    height:50,
+    backgroundColor:'blue',
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  textTitle:{
+    fontSize:25,
+    fontWeight:'bold',
+    textAlign:'left',
+    padding:20,
+    paddingTop:40,
+    color:'black'
   },
 
 });
