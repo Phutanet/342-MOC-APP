9778f5bb7c36c95a495b67a8525c688958c0d27b
real-43 <60128410+real-43@users.noreply.github.com>
2022-03-31 17:03:32 +0700

    update dont play this

--- screens/component/News.js
+++ screens/component/News.js
@@ -3,91 +3,96 @@ import { View, Text, StyleSheet } from 'react-native'
 import customData from './JSON/news'
 
 export default class News extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            dataSource: null,
+        }
+    }
 
-        constructor(props) {
-            super(props);
-            this.state = {
-                isLoading: true,
-                dataSource: null,
+    componentDidMount() {
+        return fetch ('https://dataapi.moc.go.th/ditp-activity-types')
+            .then ((response) => response.json())
+            .then ((responseJson) => {
+                this.setState({
+                    isLoading: false,
+                    dataSource: responseJson,
+                })
+                console.log("Data: ", this.state.dataSource)
+                console.log("isloading: ", this.state.isLoading)
+            })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+    render() {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.text}>
+                    <Text>Loading Data...</Text>
+                </View>
+            )
+        }
+        else {
+            { customData.map((data) =>{
+                return (
+                    <View>
+                        <View style={styles.newsContainer}>
+                        
+                                <Text style={styles.title}>
+                                    {customData.title}
+                                </Text>
+                                <Text style={styles.date}>
+                                    {customData.description}
+                                </Text>
+                                <Text style={styles.newsDescription}>
+                                    {customData.datetime}
+                                </Text>
+
+                            <Text style={styles.title}>
+                                {this.state.dataSource[1].id}
+                            </Text>
+                            <Text style={styles.date}>
+                                {this.state.dataSource[1].name_th}
+                            </Text>
+                            <Text style={styles.newsDescription}>
+                                {this.state.dataSource[1].name_en}
+                            </Text>
+
+                            <Text style={styles.title}>
+                                {this.state.dataSource[2].id}
+                            </Text>
+                            <Text style={styles.date}>
+                                {this.state.dataSource[2].name_th}
+                            </Text>
+                            <Text style={styles.newsDescription}>
+                                {this.state.dataSource[2].name_en}
+                            </Text>
+                        </View>
+                    </View>
+                );
             }
         }
+    }
+}
 
-        componentDidMount() {
-            return fetch('https://dataapi.moc.go.th/ditp-activity-types')
-                .then((response) => response.json())
-                .then((responseJson) => {
-                    this.setState({
-                        isLoading: false,
-                        dataSource: responseJson,
-                    })
-                    console.log("Data: ", this.state.dataSource)
-                    console.log("isloading: ", this.state.isLoading)
-                })
-                .catch((error) => {
-                    console.log(error)
-                })
-        }
-
-        render() {
-                if (this.state.isLoading) {
-                    return ( <
-                        View style = { styles.text } >
-                        <
-                        Text > Loading Data... < /Text> <
-                        /View>
-                    )
-                } else {
-                    {
-                        customData.map((data) => {
-                                return ( <
-                                    View >
-                                    <
-                                    View style = { styles.newsContainer } >
-
-                                    <
-                                    Text style = { styles.title } > { customData.title } <
-                                    /Text> <
-                                    Text style = { styles.date } > { customData.description } <
-                                    /Text> <
-                                    Text style = { styles.newsDescription } > { customData.datetime } <
-                                    /Text>
-
-                                    <
-                                    Text style = { styles.title } > { this.state.dataSource[1].id } <
-                                    /Text> <
-                                    Text style = { styles.date } > { this.state.dataSource[1].name_th } <
-                                    /Text> <
-                                    Text style = { styles.newsDescription } > { this.state.dataSource[1].name_en } <
-                                    /Text>
-
-                                    <
-                                    Text style = { styles.title } > { this.state.dataSource[2].id } <
-                                    /Text> <
-                                    Text style = { styles.date } > { this.state.dataSource[2].name_th } <
-                                    /Text> <
-                                    Text style = { styles.newsDescription } > { this.state.dataSource[2].name_en } <
-                                    /Text> <
-                                    /View> <
-                                    /View>
-                                );
-                            }
-                        }
-                    }
-
-                    const styles = StyleSheet.create({
-                        newsContainer: {
-                            padding: 10
-                        },
-                        title: {
-                            fontSize: 18,
-                            marginTop: 10,
-                            fontWeight: "600"
-                        },
-                        newsDescription: {
-                            fontSize: 16,
-                            marginTop: 10
-                        },
-                        date: {
-                            fontSize: 14
-                        },
-                    });
+const styles = StyleSheet.create({
+    newsContainer: {
+        padding: 10
+    },
+    title: {
+        fontSize: 18,
+        marginTop: 10,
+        fontWeight: "600"
+    },
+    newsDescription: {
+        fontSize: 16,
+        marginTop: 10
+    },
+    date: {
+        fontSize: 14
+    },
+});
