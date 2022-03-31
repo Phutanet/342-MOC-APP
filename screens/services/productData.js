1edf2f71ac6ad99c25f9074ca9e0dc25b7d86c05
pwat16918 <pwat999@hotmail.com>
2022-03-31 09:46:55 +0700

    Add GET API

--- screens/services/productData.js
+++ screens/services/productData.js
@@ -1,16 +1,17 @@
-export function getProductData(id, fromDate, toDate) {
-    const input = {
-      product_id: id,
-      from_date: fromDate,
-      to_date: toDate,
-    }
-    const url = ("https://dataapi.moc.go.th/gis-product-prices?product_id="
-                +input.product_id
-                +"&from_date="
-                +input.from_date
-                +"&to_date="
-                +input.to_date);
+export function getProductData(id) {
 
+    const date = {
+      from_date: "2022-03-29",
+      to_date: "2022-03-29",
+    }
+  
+    const url = ("https://dataapi.moc.go.th/gis-product-prices?product_id="
+      +id
+      +"&from_date="
+      +date.from_date
+      +"&to_date="
+      +date.to_date);
+  
     return fetch(url)
-      .then(data => data.json())
+      .then(data => data.json());
   }
