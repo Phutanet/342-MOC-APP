950f16683d3eac943c9c81eb41e9d601c4a72190
real-43 <60128410+real-43@users.noreply.github.com>
2022-03-31 17:01:50 +0700

    update but error dont interact with news

--- screens/component/News.js
+++ screens/component/News.js
@@ -1,96 +1,93 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-
+import customData from './JSON/news'
 
 export default class News extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoading: true,
-            dataSource: null,
+        constructor(props) {
+            super(props);
+            this.state = {
+                isLoading: true,
+                dataSource: null,
+            }
         }
-    }
 
-    componentDidMount() {
-        return fetch ('https://dataapi.moc.go.th/ditp-activity-types')
-            .then ((response) => response.json())
-            .then ((responseJson) => {
-                this.setState({
-                    isLoading: false,
-                    dataSource: responseJson,
+        componentDidMount() {
+            return fetch('https://dataapi.moc.go.th/ditp-activity-types')
+                .then((response) => response.json())
+                .then((responseJson) => {
+                    this.setState({
+                        isLoading: false,
+                        dataSource: responseJson,
+                    })
+                    console.log("Data: ", this.state.dataSource)
+                    console.log("isloading: ", this.state.isLoading)
                 })
-                console.log("Data: ", this.state.dataSource)
-                console.log("isloading: ", this.state.isLoading)
-            })
-        .catch((error) => {
-            console.log(error)
-        })
-    }
-
-    render() {
-        if (this.state.isLoading) {
-            return (
-                <View style={styles.text}>
-                    <Text>Loading Data...</Text>
-                </View>
-            )
+                .catch((error) => {
+                    console.log(error)
+                })
         }
-        else {
 
-            return (
-                <View>
-                    <View style={styles.newsContainer}>
-                        <Text style={styles.title}>
-                            {this.state.dataSource[0].id}
-                        </Text>
-                        <Text style={styles.date}>
-                            {this.state.dataSource[0].name_th}
-                        </Text>
-                        <Text style={styles.newsDescription}>
-                            {this.state.dataSource[0].name_en}
-                        </Text>
+        render() {
+                if (this.state.isLoading) {
+                    return ( <
+                        View style = { styles.text } >
+                        <
+                        Text > Loading Data... < /Text> <
+                        /View>
+                    )
+                } else {
+                    {
+                        customData.map((data) => {
+                                return ( <
+                                    View >
+                                    <
+                                    View style = { styles.newsContainer } >
 
-                        <Text style={styles.title}>
-                            {this.state.dataSource[1].id}
-                        </Text>
-                        <Text style={styles.date}>
-                            {this.state.dataSource[1].name_th}
-                        </Text>
-                        <Text style={styles.newsDescription}>
-                            {this.state.dataSource[1].name_en}
-                        </Text>
+                                    <
+                                    Text style = { styles.title } > { customData.title } <
+                                    /Text> <
+                                    Text style = { styles.date } > { customData.description } <
+                                    /Text> <
+                                    Text style = { styles.newsDescription } > { customData.datetime } <
+                                    /Text>
 
-                        <Text style={styles.title}>
-                            {this.state.dataSource[2].id}
-                        </Text>
-                        <Text style={styles.date}>
-                            {this.state.dataSource[2].name_th}
-                        </Text>
-                        <Text style={styles.newsDescription}>
-                            {this.state.dataSource[2].name_en}
-                        </Text>
-                    </View>
-                </View>
-            );
-        }
-    }
-}
+                                    <
+                                    Text style = { styles.title } > { this.state.dataSource[1].id } <
+                                    /Text> <
+                                    Text style = { styles.date } > { this.state.dataSource[1].name_th } <
+                                    /Text> <
+                                    Text style = { styles.newsDescription } > { this.state.dataSource[1].name_en } <
+                                    /Text>
 
-const styles = StyleSheet.create({
-    newsContainer: {
-        padding: 10
-    },
-    title: {
-        fontSize: 18,
-        marginTop: 10,
-        fontWeight: "600"
-    },
-    newsDescription: {
-        fontSize: 16,
-        marginTop: 10
-    },
-    date: {
-        fontSize: 14
-    },
-});
+                                    <
+                                    Text style = { styles.title } > { this.state.dataSource[2].id } <
+                                    /Text> <
+                                    Text style = { styles.date } > { this.state.dataSource[2].name_th } <
+                                    /Text> <
+                                    Text style = { styles.newsDescription } > { this.state.dataSource[2].name_en } <
+                                    /Text> <
+                                    /View> <
+                                    /View>
+                                );
+                            }
+                        }
+                    }
+
+                    const styles = StyleSheet.create({
+                        newsContainer: {
+                            padding: 10
+                        },
+                        title: {
+                            fontSize: 18,
+                            marginTop: 10,
+                            fontWeight: "600"
+                        },
+                        newsDescription: {
+                            fontSize: 16,
+                            marginTop: 10
+                        },
+                        date: {
+                            fontSize: 14
+                        },
+                    });
