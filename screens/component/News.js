26dbaac73908fc92be63d019c289e0e5dad52525
real-43 <60128410+real-43@users.noreply.github.com>
2022-04-01 14:24:35 +0700

    add home banner

--- screens/component/News.js
+++ screens/component/News.js
@@ -3,55 +3,26 @@ import { View, Text, StyleSheet, Image } from 'react-native'
 import customData from './JSON/news'
 
 export default class News extends React.Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoading: true,
-            dataSource: null,
-        }
-    }
 
-    componentDidMount() {
-        return fetch ('https://dataapi.moc.go.th/ditp-activity-types')
-            .then ((response) => response.json())
-            .then ((responseJson) => {
-                this.setState({
-                    isLoading: false,
-                    dataSource: responseJson,
-                })
-                console.log("Data: ", this.state.dataSource)
-                console.log("isloading: ", this.state.isLoading)
-            })
-        .catch((error) => {
-            console.log(error)
-        })
-    }
-
-    render() {
-        if (this.state.isLoading) {
-            return (
-                <View style={styles.text}>
-                    <Text>Loading Data...</Text>
-                </View>
-            )
-        }
-
-        else {
-            return (
-                <View>
-                    { customData.map((data) =>{
-                        return(
-                            <>
-                            <Text>{data.title}</Text>
-                            <Image source = {{uri: data.photo }}style = {{ width: 300, height: 300, marginLeft:'auto', marginRight:'auto' }} />
-                            </>
-                        )})}
-                </View>
-            );
-        }
+    render() {        
+        return (
+            <View>
+                <Image
+                style={styles.image}
+                source={require('.././image/HomwBanner.jpg')}
+            />
+                { customData.map((data) =>{
+                    return(
+                        <>
+                        <Text>{data.title}</Text>
+                        <Image source = {{uri: data.photo }}style = {styles.image} />
+                        </>
+                    )})}
+            </View>
+        );
     }
 }
+
 
 const styles = StyleSheet.create({
     newsContainer: {
@@ -69,4 +40,10 @@ const styles = StyleSheet.create({
     date: {
         fontSize: 14
     },
+    image: { 
+        width: 300, 
+        height: 300, 
+        marginLeft:'auto', 
+        marginRight:'auto' 
+    }
 });
