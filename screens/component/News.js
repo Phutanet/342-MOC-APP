e11b280ee67ff36026848917cd6e54c2947ab484
real-43 <60128410+real-43@users.noreply.github.com>
2022-04-03 18:21:43 +0700

    dont have page sep

--- screens/component/News.js
+++ screens/component/News.js
@@ -1,35 +1,42 @@
 import React from 'react'
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { SafeAreaView, View, Text, StyleSheet, Image } from 'react-native'
 import customData from './JSON/news'
+import ReadMore from '@fawazahmed/react-native-read-more';
 
 export default class News extends React.Component {
 
-    render() {        
-        return (
-            <View>
-                <Image
-                    style={styles.banner}
-                    source={require('.././image/HomwBanner.jpg')}
-                />
-                { customData.map((data) =>{
-                    return(
-                        <>
-                        <View style = {styles.row}>
-                            <Image 
-                            source={require('../image/mocLogo.png')} 
-                            style={styles.box1} 
-                            />
-                            <Text style = {styles.box2} >
-                                <Text style = {{color: 'black'}}>{data.title}{"\n"}</Text>
-                                <Text>{"\n"}</Text>
-                                <Text style = {{marginTop:'10'}}>{data.datetime}</Text>
-                            </Text>
-                        </View>
-                        </>
+    render() {
+        return ( <
+            View >
+            <
+            Image style = { styles.banner }
+            source = { require('.././image/HomwBanner.jpg') }
+            />  {
+                customData.reverse().map((data) => {
+                    return ( <
+                        >
+                        <
+                        View style = { styles.row } >
+                        <
+                        Image source = { require('../image/mocLogo.png') }
+                        style = { styles.box1 }
+                        />   <
+                        Text style = { styles.box2 } >
+                        <
+                        Text style = {
+                            { color: 'black' } } > { data.title } { "\n" } < /Text>   <
+                        Text > { "\n" } < /Text>   <
+                        Text > { data.datetime } < /Text>  <
+                        /Text>  <
+                        Text > { data.description } < /Text>   <
+                        /View>   <
+                        />
                     )
-                })}
+                })
+            }
 
-            </View>
+            <
+            /View>
         );
     }
 }
@@ -51,15 +58,15 @@ const styles = StyleSheet.create({
     date: {
         fontSize: 14
     },
-    image: { 
-        width: 350, 
-        height: 320, 
+    image: {
+        width: 350,
+        height: 320,
         justifyContent: 'center',
         alignItems: 'center',
     },
     banner: {
-        width: 450, 
-        height:140,
+        width: 450,
+        height: 140,
         opacity: 0.8
     },
     box1: {
