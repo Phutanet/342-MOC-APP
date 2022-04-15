15e3cb07925387676238596e441da17ec0284bcd
real-43 <60128410+real-43@users.noreply.github.com>
2022-04-15 14:05:17 +0700

    dooing sonkiat request about scroll bar

--- screens/component/News.js
+++ screens/component/News.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SafeAreaView, View, Text, StyleSheet, Image ,ScrollView,ImageBackground} from 'react-native'
+import { SafeAreaView, View, Text, StyleSheet, Image ,ScrollView,ImageBackground,Animated} from 'react-native'
 import customData from './JSON/news'
 import ReadMore from 'react-native-read-more-text';
 
@@ -9,58 +9,55 @@ export default class News extends React.Component {
     render() {
         return ( 
             <View>
-            <ImageBackground source={require('../image/newsBanner1.jpg')} resizeMode="cover" style={styles.image}>
-              <Text style={styles.textHeader}>ข่าวประชาสัมพันธ์</Text>
-              </ImageBackground>
-              <View style={{color:'#00ffffff'}}>
-                 <View style={styles.cardContract}>
+                <ImageBackground source={require('../image/newsBanner1.jpg')} resizeMode="cover" style={styles.image}>
+                    <Text style={styles.textHeader}>ข่าวประชาสัมพันธ์</Text>
+                </ImageBackground>
+                <View style={{color:'#00ffffff'}}>
+                    <View style={styles.cardContract}>
                 
-            {customData.reverse().map((data) => { 
-                    return (
-                    
-                        <View style={{alignItems:'center',paddingBottom:20,}}>
-                            {/* Topic NEWS  --> red*/}
-                            <View style = { styles.row } >
-                                <Image 
-                                    source = { require('../image/mocLogo.png') }
-                                    style = { styles.box1 }
-                                />  
-                                <Text style = { styles.box2 } >
-                                    <Text style = {{ color: 'black' } } >{data.title } </Text>   
-                                    <Text >{ "\n" } </Text>    
-                                </Text> 
-                            </View>  
+                        {customData.reverse().map((data) => { 
+                                return (
+                                
+                                    <View style={{alignItems:'center',paddingBottom:20,}}>
+                                        {/* Topic NEWS  --> red*/}
+                                        <View style = { styles.row } >
+                                            <Image 
+                                                source = { require('../image/mocLogo.png') }
+                                                style = { styles.box1 }
+                                            />  
+                                            <Text style = { styles.box2 } >
+                                                <Text style = {{ color: 'black' } } >{data.title } </Text>   
+                                                <Text >{ "\n" } </Text>    
+                                            </Text> 
+                                        </View>  
 
-                            {/* Content NEWS */}
-                            <SafeAreaView style={styles.safe}>
-                                <View style={styles.root}>
-                                    <ReadMore
-                                    numberOfLines={3}
-                                    renderTruncatedFooter={this._renderTruncatedFooter}
-                                    renderRevealedFooter={this._renderRevealedFooter}
-                                    onReady={this._handleTextReady}>
-                                        <Text>
-                                            {data.desciption}
-                                        </Text>
-                                    </ReadMore>
-                                </View>
-                            </SafeAreaView>
-                            <View style={{flexDirection: 'row', alignItems: 'center', padding: 10}}>
-                                <View style={{flex: 1, height: 1, backgroundColor: 'black'}} />
-                            </View>
-                        </View>
-                   
-                        
-                    )
-                }
-                
-                )
-            }
+                                        {/* Content NEWS */}
+                                        <SafeAreaView style={styles.safe}>
+                                            <View style={styles.root}>
+                                                <ReadMore
+                                                numberOfLines={3}
+                                                renderTruncatedFooter={this._renderTruncatedFooter}
+                                                renderRevealedFooter={this._renderRevealedFooter}
+                                                onReady={this._handleTextReady}>
+                                                    <Text>
+                                                        {data.desciption}
+                                                    </Text>
+                                                </ReadMore>
+                                            </View>
+                                        </SafeAreaView>
+                                        <View style={{flexDirection: 'row', alignItems: 'center', padding: 10}}>
+                                            <View style={{flex: 1, height: 1, backgroundColor: 'black'}} />
+                                        </View>
+                                    </View>
+                    
+                            
+                                )
+                            }
+                    
+                        )}
            
-            </View> 
-              </View>
-            
-             
+                    </View> 
+                </View>
             </View>
         );
     }
